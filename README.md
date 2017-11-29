d4c3428a5c1f499baa20112bcfb67a1785badc4e
李锦 <lijin@webull.com>
2017-11-29 11:31:22 +0800

    pipeLine

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # RedisTest
+
+1.redis基本操作
+2.操作日志 自定义annotation
--- src/test/java/com/redis/test/pool/RedisTest.java
+++ src/test/java/com/redis/test/pool/RedisTest.java
@@ -1,7 +1,10 @@
 package com.redis.test.pool;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.junit.Test;
@@ -12,6 +15,7 @@ import com.redis.test.junit.AbstractTest;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Pipeline;
 import redis.clients.jedis.SortingParams;
 
 public class RedisTest extends AbstractTest {
@@ -52,6 +56,35 @@ public class RedisTest extends AbstractTest {
 		
 		List<String> list1 = redisCache.sort("today_cost");
 		System.out.println(list1);
+	}
+	
+	private static final int HASH_EXPIRE = 30;
+	private static final int KEYS_EXPIRE = 60;
+	@Test
+	public void testJedisMutiOperate() {
+		String key1 = "key1";
+		String key2 = "key2";
+		try (Jedis jedis = jedisPool.getResource(); Pipeline pipeline = jedis.pipelined()) {
+			Map<String, String> hash = new HashMap<>();
+			hash.put("mKey", "mValue");
+			pipeline.hmset(key1, hash);
+			pipeline.expire(key1, HASH_EXPIRE);
+			
+			pipeline.sadd(key2, key2);
+			pipeline.expire(key2, KEYS_EXPIRE);
+			pipeline.sync();
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
+		Set<String> set = redisCache.keys("*");
+		System.out.println(set);
+	}
+	
+	
+	@Test
+	public void testShowAllKeys() {
+		Set<String> set = redisCache.keys("*");
+		System.out.println(set);
 	}
 }
  
